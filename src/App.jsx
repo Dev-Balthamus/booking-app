8cec1e490a2930937eace835de21b61d1eb7d3c8
Edoardo <edoardoderosaa@gmail.com>
2024-05-27 12:22:49 +0200

    aggiornamento navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Splash from "./components/Splash";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <main id="app-container">
       <Splash />
+      <Navbar />
     </main>
   );
 }
