9087cff1ab6003bafdcb810993eb65d22341e456
Federica <fedecprinzi@gmail.com>
2024-05-30 16:35:30 +0200

    updating_summary_css_F

--- src/components/BookingSummary.jsx
+++ src/components/BookingSummary.jsx
@@ -14,41 +14,45 @@ export default function BookingSummary() {
 
   return (
     <div className="orderDetails">
-      <h3>Order Details</h3>
-      <ul>
-        <li>
-          <span className="info-label">Car:</span>
-          <span className="info-value">{info.car}</span>
-        </li>
-        <li>
-          <span className="info-label">Car Model:</span>
-          <span className="info-value">{info.model}</span>
-        </li>
-        <li>
-          <span className="info-label">Pickup Date:</span>
-          <span className="info-value">{info.pickup}</span>
-        </li>
-        <li>
-          <span className="info-label">Return Date:</span>
-          <span className="info-value">{info.return}</span>
-        </li>
-        <li>
-          <span className="info-label">Location:</span>
-          <span className="info-value">{info.location}</span>
-        </li>
-        <li>
-          <span className="info-label">Price:</span>
-          <span className="info-value">{info.price}</span>
-        </li>
-        <li>
-          <span className="info-label">Tax:</span>
-          <span className="info-value">{info.tax}</span>
-        </li>
-        <li>
-          <span className="info-label">Total Payable:</span>
-          <span className="info-value">{info.total}</span>
-        </li>
-      </ul>
+      <div className="outer">
+        <h3>Order Details</h3>
+        <div className="outerBox">
+          <ul>
+            <li>
+              <span className="info-label">Car:</span>
+              <span className="info-value">{info.car}</span>
+            </li>
+            <li>
+              <span className="info-label">Car Model:</span>
+              <span className="info-value">{info.model}</span>
+            </li>
+            <li>
+              <span className="info-label">Pickup Date:</span>
+              <span className="info-value">{info.pickup}</span>
+            </li>
+            <li>
+              <span className="info-label">Return Date:</span>
+              <span className="info-value">{info.return}</span>
+            </li>
+            <li>
+              <span className="info-label">Location:</span>
+              <span className="info-value">{info.location}</span>
+            </li>
+            <li>
+              <span className="info-label">Price:</span>
+              <span className="info-value">{info.price}</span>
+            </li>
+            <li>
+              <span className="info-label">Tax:</span>
+              <span className="info-value">{info.tax}</span>
+            </li>
+            <li>
+              <span className="info-label">Total Payable:</span>
+              <span className="info-value">{info.total}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
       <button className="horizontal-padding button-primary">
         Rent Car Now
       </button>
