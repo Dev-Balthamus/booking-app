d8fb715a5d0bac311e878346f57dcf3b591fdf49
scumenez <cuomo.riccardo@gmail.com>
2024-05-30 17:05:50 +0200

    Data Form Changes

--- src/components/DataForm.jsx
+++ src/components/DataForm.jsx
@@ -32,30 +32,48 @@ export default function DataForm() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth="1.5"
+          stroke-width="1.5"
           stroke="currentColor"
-          className="w-6 h-6"
-          width="1rem"
+          class="size-6"
         >
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            stroke-linecap="round"
+            stroke-linejoin="round"
             d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
           />
         </svg>
       </div>
       <div>
-        <input type="date" className="pickupd" ref={pickupDateRef} />
-        <div className="placeholder">Pickup Date</div>
+        <input
+          type="text"
+          className="pickupd"
+          ref={pickupDateRef}
+          placeholder="Pickup Date"
+        />
       </div>
       <div>
-        <input type="time" className="pickupt" ref={pickupTimeRef} />
+        <input
+          type="text"
+          className="pickupt"
+          ref={pickupTimeRef}
+          placeholder="Time"
+        />
       </div>
       <div>
-        <input type="date" className="returnd" ref={returnDateRef} />
+        <input
+          type="text"
+          className="returnd"
+          ref={returnDateRef}
+          placeholder="Return Date"
+        />
       </div>
       <div>
-        <input type="time" className="returnt" ref={returnTimeRef} />
+        <input
+          type="text"
+          className="returnt"
+          ref={returnTimeRef}
+          placeholder="Time"
+        />
       </div>
       <div className="location">
         <button className="horizontal-padding button-primary">
