ed52827fc8ac1d5f67c031868dc1b03021dba5de
Francesco <francesco7411@gmail.com>
2024-06-04 02:37:44 +0200

    Upload Finished Splash_FraBa

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -3,28 +3,26 @@ import "../assets/css/Splash.css";
 export default function Splash({ setPage }) {
   return (
     <div id="main-wrapper">
-      <section id="image-container">
+      <section id="image-container" className="horizontal-padding">
         <img
-          className="horizontal-padding"
           src="/AutoPrincipale.png"
           width="640"
           height="290"
           alt="Immagine Auto Principale"
         />
       </section>
-      <section id="text-plus-button">
-        <div className="horizontal-padding">
+      <section>
+        <div id="splash-text" className="horizontal-padding">
           <h1>Find Ideal Car for Your Next Trip</h1>
           <p>
             The point of using. Lorem ipsum isthat Readable content of a page
           </p>
         </div>
-        <button
-          className="horizontal-padding button-primary"
-          onClick={() => setPage("booking")}
-        >
-          Get Started
-        </button>
+        <div id="splash-button" className="horizontal-padding">
+          <button className="button-primary" onClick={() => setPage("booking")}>
+            Get Started
+          </button>
+        </div>
       </section>
     </div>
   );
